ca9d08cea13448c7107f9e55a11a02b9748a3c58
nick van der burgt <n.vanderburgt@student.fontys.nl>
2018-09-19 11:50:11 +0200

    Repository Template Created
    Repository Template Created

--- src/main/Java/DAL/Interfaces/IUserRepository.java
+++ src/main/Java/DAL/Interfaces/IUserRepository.java
@@ -1,4 +1,14 @@
 package DAL.Interfaces;
 
+import Domain.Account;
+import Domain.StorageType;
+
+import java.util.Date;
+
 public interface IUserRepository {
+    Account GetUser(int id);
+    Boolean CreateUser(String username, String password, String email, String TelephoneNumber , Date DateOfBirth);
+    Boolean DeleteUser(int id);
+    Boolean UpdateUser(Account account);
+    void ChangeType(StorageType type);
 }
--- src/main/Java/DAL/Repositories/UserRepository.java
+++ src/main/Java/DAL/Repositories/UserRepository.java
@@ -1,9 +1,42 @@
 package DAL.Repositories;
 
+import DAL.Interfaces.IUserContext;
 import DAL.Interfaces.IUserRepository;
+import Domain.Account;
+import Domain.StorageType;
+
+import java.util.Date;
 
 public class UserRepository implements IUserRepository {
-    public UserRepository() {
+    private IUserContext _context;
 
+    public UserRepository() {
+        //Met factory context opvragen
+
+    }
+
+    @Override
+    public Account GetUser(int id) {
+        return _context.GetUser(id);
+    }
+
+    @Override
+    public Boolean CreateUser(String username, String password, String email, String TelephoneNumber, Date DateOfBirth) {
+        return _context.CreateUser(username, password, email, TelephoneNumber, DateOfBirth);
+    }
+
+    @Override
+    public Boolean DeleteUser(int id) {
+        return _context.DeleteUser(id);
+    }
+
+    @Override
+    public Boolean UpdateUser(Account account) {
+        return _context.UpdateUser(account);
+    }
+
+    @Override
+    public void ChangeType(StorageType type) {
+        //Factory.changetype(type);
     }
 }
