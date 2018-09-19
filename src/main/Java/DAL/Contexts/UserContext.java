4d6c14914d7aba7172197b6aa433da149689e20f
nick van der burgt <n.vanderburgt@student.fontys.nl>
2018-09-19 11:31:03 +0200

    Test
    Test

--- src/main/Java/DAL/Contexts/UserContext.java
+++ src/main/Java/DAL/Contexts/UserContext.java
@@ -1,9 +1,10 @@
 package DAL.Contexts;
 
 import DAL.Interfaces.IUserContext;
+import Domain.Account;
 
 public class UserContext implements IUserContext {
     public UserContext() {
-
+        Account GetUser(int id);
     }
 }
--- src/main/Java/DAL/Interfaces/IUserContext.java
+++ src/main/Java/DAL/Interfaces/IUserContext.java
@@ -1,5 +1,11 @@
 package DAL.Interfaces;
 
-public interface IUserContext {
+import Domain.Account;
 
+import java.util.Date;
+
+public interface IUserContext {
+    Account GetUser(int id);
+    Boolean CreateUser(String username, String password, String email, String TelephoneNumber ,Date DateOfBirth);
+    Boolean DeleteUser(int id);
 }
