5e5dec6cf1c5e011a13eef9ff3af71ed563fd1fc
nick van der burgt <n.vanderburgt@student.fontys.nl>
2018-10-10 09:42:32 +0200

    documentatie toegevoegd aan exception handler

--- Web/src/main/java/com/kiddybank/utils/RestErrorHandler.java
+++ Web/src/main/java/com/kiddybank/utils/RestErrorHandler.java
@@ -12,13 +12,24 @@ import java.util.Date;
 @ControllerAdvice
 public class RestErrorHandler extends ResponseEntityExceptionHandler {
 
+    /**
+     * Illegalargumentexception opvanger, deze wordt gebruikt als de client verkeerde parameters meestuurt of bijvoorbeeld een onbestaande accountID meestuurt.
+     * @param e the exception that was thrown by the request
+     * @param request the webrequest containing information like the url
+     * @return a responseentity of type errordetails with HTTP status 400
+     */
     @ExceptionHandler(value = IllegalArgumentException.class)
     public final ResponseEntity<ErrorDetails> handleIllegalArgumentException (IllegalArgumentException e, WebRequest request) {
         ErrorDetails details = new ErrorDetails(new Date(), e.getMessage(), request.getDescription(false));
         return new ResponseEntity<>(details, HttpStatus.BAD_REQUEST);
     }
 
-    //globale error handler
+    /**
+     * Exception opvanger, deze vangt alle overige excepties die niet specifiek afgehandeld worden.
+     * @param e the exception that was thrown by the request
+     * @param request the webrequest containing information like the url
+     * @return a responsentity of type errordetails with HTTP status 500
+     */
     @ExceptionHandler(value = Exception.class)
     public ResponseEntity<ErrorDetails> handleExpection(Exception e, WebRequest request) {
         ErrorDetails details = new ErrorDetails(new Date(), "Unknown error, please contact support.", request.getDescription(false));
