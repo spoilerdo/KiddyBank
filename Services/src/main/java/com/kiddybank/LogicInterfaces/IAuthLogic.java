3029f54a16e499769f8e90f48cbd0b48b82fc744
nick van der burgt <n.vanderburgt@student.fontys.nl>
2018-10-23 20:23:10 +0200

    Kleine bugfixes, swagger aanpassing
    Pushing to master IM GOING IN

--- Services/src/main/java/com/kiddybank/LogicInterfaces/IAuthLogic.java
+++
@@ -1,9 +0,0 @@
-package com.kiddybank.LogicInterfaces;
-
-import com.kiddybank.Entities.Account;
-
-import javax.security.auth.login.FailedLoginException;
-
-public interface IAuthLogic {
-    String SignIn(Account account) throws FailedLoginException;
-}
