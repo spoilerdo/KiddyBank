4b0e8cfcf84eec942e867bb822c4fd9aa365c837
nick van der burgt <n.vanderburgt@student.fontys.nl>
2018-10-23 12:19:17 +0200

    Cors gecontroleerd en aangezet
    Cors gecontroleerd en aangezet

--- Web/src/main/java/com/kiddybank/Security/WebSecurity.java
+++ Web/src/main/java/com/kiddybank/Security/WebSecurity.java
@@ -2,6 +2,7 @@ package com.kiddybank.Security;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Bean;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -10,6 +11,9 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import java.util.Arrays;
 import java.util.List;
@@ -33,7 +37,6 @@ public class WebSecurity extends WebSecurityConfigurerAdapter {
         this.bCryptPasswordEncoder = bCryptPasswordEncoder;
 
         //Create list of CORS ALlOWED METHODS AND ORIGINS
-        allowedOrigins = Arrays.asList("*");
         allowedMethods = Arrays.asList("HEAD", "GET", "POST", "PUT", "DELETE", "PATCH");
         allowedHeaders = Arrays.asList("Authorization");
     }
@@ -50,7 +53,7 @@ public class WebSecurity extends WebSecurityConfigurerAdapter {
                 // this disables session creation on Spring Security
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
 
-        //http.cors();
+        http.cors();
     }
 
     @Override
@@ -58,16 +61,15 @@ public class WebSecurity extends WebSecurityConfigurerAdapter {
         auth.userDetailsService(userDetailsImpl).passwordEncoder(bCryptPasswordEncoder);
     }
 
-    /*
+
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         final CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(allowedOrigins);
         configuration.setAllowedMethods(allowedMethods);
         configuration.setAllowCredentials(true);
         configuration.setAllowedHeaders(allowedHeaders);
         final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", configuration);
         return source;
-    } */
+    }
 }
