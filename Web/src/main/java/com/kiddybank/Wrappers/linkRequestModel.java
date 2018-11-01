1d28203eb44385c29cfb60c667b29f9dfb53bd33
nick van der burgt <n.vanderburgt@student.fontys.nl>
2018-11-01 20:26:04 +0100

    push v2.0

--- Web/src/main/java/com/kiddybank/Wrappers/linkRequestModel.java
+++ Web/src/main/java/com/kiddybank/Wrappers/linkRequestModel.java
@@ -1,4 +1,28 @@
 package com.kiddybank.Wrappers;
 
+//wrapper for linking bank accounts to your own bank account ( own account comes from principal claim in different parameter ).
 public class linkRequestModel {
+    private int otherID;
+    private int bankID;
+
+    public linkRequestModel() {
+
+    }
+
+
+    public int getOtherID() {
+        return otherID;
+    }
+
+    public void setOtherID(int otherID) {
+        this.otherID = otherID;
+    }
+
+    public int getBankID() {
+        return bankID;
+    }
+
+    public void setBankID(int bankID) {
+        this.bankID = bankID;
+    }
 }
