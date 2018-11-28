6e22ea0fd81040dc1c7312195909a0af36ef6def
nick van der burgt <n.vanderburgt@student.fontys.nl>
2018-11-28 10:49:00 +0100

    Fixed sign up url
    yeet

--- Web/src/main/java/com/kiddybank/Security/SecurityConstants/SecurityConstants.java
+++ Web/src/main/java/com/kiddybank/Security/SecurityConstants/SecurityConstants.java
@@ -4,5 +4,5 @@ public final class SecurityConstants {
     public static final String JWTKEY = "jadwbdjwbajidbawbdubaousnojijwi0ajiojipsjdkmmsadmnnajkwndjnwhu9u9hhu9huomahid73gi7qgdi";
     public static final String TOKEN_PREFIX = "Bearer ";
     public static final String HEADER_STRING = "Authorization";
-    public static final String SIGN_UP_URL = "/account/add";
+    public static final String SIGN_UP_URL = "/accounts";
 }
